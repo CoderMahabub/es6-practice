09a248c98ce4abb026063b0f7dba6bc02c1d4b5b
CoderMahabub <codermahabub@gmail.com>
2023-02-09 02:33:56 +0600

    Backtick

--- destructuting.js
+++ destructuting.js
@@ -1,20 +1,23 @@
-// 8. Destructuring 
-//৮. সিম্পল একটা জাভাস্ক্রিপ্ট অবজেক্ট এর কোন একটা প্রোপার্টিকে ভেরিয়েবল হিসেবে ডিক্লেয়ার করার জন্য destructuring ইউজ করো। 
+// 8. Destructuring
+//৮. সিম্পল একটা জাভাস্ক্রিপ্ট অবজেক্ট এর কোন একটা প্রোপার্টিকে ভেরিয়েবল হিসেবে ডিক্লেয়ার করার জন্য destructuring ইউজ করো।
 const fish = {
-    id: 58,
-    name: 'King Hilsha',
-    price: 9000,
-    phone: '01555555555',
-    address: 'Chandpur',
-    dress: 'Silver',
-    pukur: {
-        kiase: 'mach',
-        kotogula: 500,
-        ojon: '1kg'
-    }
+	id: 58,
+	name: 'King Hilsha',
+	price: 9000,
+	phone: '01555555555',
+	address: 'Chandpur',
+	dress: 'Silver',
+	pukur: {
+		kiase: 'mach',
+		kotogula: 500,
+		ojon: '1kg'
+	}
 };
 
-const { address, phone, price, dress, id } = fish;
+const { id, name, price, pukur } = fish;
+console.log(pukur);
+
+// const { address, phone, price, dress, id } = fish;
 const { kiase, kotogula } = fish.pukur;
 
 // console.log(phone, price);
@@ -28,9 +31,8 @@ const { x, b } = myObject;
 // console.log(x, b);
 
 // 9. destructuring an array
-//৯. [চ্যালেঞ্জিং] array এর destructuring করবে আর সেটা করার জন্য তুমি এরে এর থার্ড পজিশন এর উপাদান কে destructuring করে 'three' নামক একটা ভেরিয়েবল এ রাখবে। 
+//৯. [চ্যালেঞ্জিং] array এর destructuring করবে আর সেটা করার জন্য তুমি এরে এর থার্ড পজিশন এর উপাদান কে destructuring করে 'three' নামক একটা ভেরিয়েবল এ রাখবে।
 // const [p, q] = [45, 65, 34, 44];
 // console.log(p, q);
-const [best, faltu, three] = ['Manush', 'Kafer', 'My X'];
+const [ best, faltu, three ] = [ 'Manush', 'Kafer', 'My X' ];
 // console.log(three);
-
---
+++ spread.js
@@ -0,0 +1,8 @@
+const max = Math.max(12, 85, 99);
+// console.log(max);
+
+const numbers = [ 12, 85, 45, 4, 2, 85, 58 ];
+const largest = Math.max(...numbers);
+console.log(largest);
+// console.log(numbers);
+// console.log(...numbers);
