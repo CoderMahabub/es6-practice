2d82956d220dfdadecd4c9d14ce8f258ea1e226f
CoderMahabub <codermahabub@gmail.com>
2023-02-10 12:16:18 +0600

    End

---
+++ forEach.js
@@ -0,0 +1,14 @@
+const friends = [ 'Tom Hanks', 'Tom Cruise', 'Tom Doe', 'Tom Solaiman' ];
+
+friends.forEach((friend) => {
+	// console.log(friend);
+});
+
+const products = [
+	{ id: 1, name: 'laptop', price: 45000 },
+	{ id: 2, name: 'iphone', price: 80500 },
+	{ id: 3, name: 'mobile', price: 4500 },
+	{ id: 4, name: 'watch', price: 4000 },
+	{ id: 5, name: 'tablet', price: 2500 }
+];
+products.forEach((product) => console.log(products));
--- map.js
+++ map.js
@@ -8,7 +8,21 @@ const half = numbers.map((n) => n / 2);
 const double = numbers.map((num) => num * 2);
 // console.log(half, double);
 
-const friends = [ 'abd', 'bcd', 'def', 'fgh' ];
+const friends = [ 'Tom Hanks', 'Tom Cruise', 'Tom Doe', 'Tom Solaiman' ];
 const friend = friends.map((friend) => friend[0]);
 const NameLength = friends.map((friend) => friend.length);
-console.log(NameLength);
+// console.log(NameLength);
+
+const products = [
+	{ id: 1, name: 'laptop', price: 45000 },
+	{ id: 2, name: 'iphone', price: 80500 },
+	{ id: 3, name: 'mobile', price: 4500 },
+	{ id: 4, name: 'watch', price: 4000 },
+	{ id: 5, name: 'tablet', price: 2500 }
+];
+
+// const items = products.map((product) => console.log(product.name));
+const items = products.map((product) => product.name);
+const prices = products.map((p) => p.price);
+// console.log(items);
+// console.log(prices);
